4c8db18b3cf3d0faf9dcc25c417b6c12f1c47d5c
Denis S <denis13th@gmail.com>
2024-04-17 17:43:39 +0300

    Remove input parameter from pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo "<< Start pipeline.sh >>"
 echo " "
-python data_creation.py $1
-python data_preprocessing.py $1
-python model_preparation.py $1
-python model_testing.py $1
+python data_creation.py
+python data_preprocessing.py
+python model_preparation.py
+python model_testing.py
 echo " "
 echo "<< Finish pipeline.sh >>"
