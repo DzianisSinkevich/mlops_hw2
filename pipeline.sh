d04c2f78e3d70b44a9237c4a3bfcf02490dac45b
Denis S <denis13th@gmail.com>
2024-04-17 17:39:15 +0300

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -2,7 +2,7 @@
 echo "<< Start pipeline.sh >>"
 echo " "
 python data_creation.py $1
-python model_preprocessing.py $1
+python data_preprocessing.py $1
 python model_preparation.py $1
 python model_testing.py $1
 echo " "
